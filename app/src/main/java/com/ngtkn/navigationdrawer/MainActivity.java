690f502c4d485d9907bf0e8a9d8f5e7668b57514
ngt kn <nagata.ken78@gmail.com>
2018-06-25 21:26:18 -0700

    adds a few fragments to demo the navigation drawer

--- app/src/main/java/com/ngtkn/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/ngtkn/navigationdrawer/MainActivity.java
@@ -1,5 +1,10 @@
 package com.ngtkn.navigationdrawer;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.net.Network;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,17 +14,20 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navigationView = findViewById(R.id.navigation_view);
         drawerLayout = findViewById(R.id.drawer);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(navigationView);
     }
 
     @Override
@@ -28,5 +36,45 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void selectItemDrawer(MenuItem menuItem){
+        android.support.v4.app.Fragment fragment = null;
+        Class fragmentClass;
+
+        switch (menuItem.getItemId()){
+            case R.id.dashboard:
+                fragmentClass = Dashboard.class;
+                break;
+            case R.id.events:
+                fragmentClass = Events.class;
+                break;
+            case R.id.search:
+                fragmentClass = Search.class;
+                break;
+            default:
+                fragmentClass = Dashboard.class;
+                break;
+        }
+        try {
+            fragment = (android.support.v4.app.Fragment) fragmentClass.newInstance();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawerLayout.closeDrawers();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItemDrawer(item);
+                return true;
+            }
+        });
     }
 }
